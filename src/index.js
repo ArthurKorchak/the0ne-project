079f106ed779ae865209ce2cea2a634da8563154
ArthurKorchak <105571446+ArthurKorchak@users.noreply.github.com>
2022-07-01 13:18:44 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
-(() => {
-    const menuBtnRef = document.querySelector("[data-menu-button]");
-    const mobileMenuRef = document.querySelector("[data-menu]");
+  (() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-menu-button]"),
+    closeModalBtn_1: document.querySelector("[data-menu-close-1]"),
+    closeModalBtn_2: document.querySelector("[data-menu-close-2]"),
+    closeModalBtn_3: document.querySelector("[data-menu-close-3]"),
+    closeModalBtn_4: document.querySelector("[data-menu-close-4]"),
+    closeModalBtn_5: document.querySelector("[data-menu-close-5]"),
+    modal: document.querySelector("[data-menu]"),
+    modal_btn: document.querySelector("[data-button]"),
+  };
 
-    menuBtnRef.addEventListener("click", () => {
+  refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn_1.addEventListener("click", toggleModal);
+    refs.closeModalBtn_2.addEventListener("click", toggleModal);
+    refs.closeModalBtn_3.addEventListener("click", toggleModal);
+    refs.closeModalBtn_4.addEventListener("click", toggleModal);
+    refs.closeModalBtn_5.addEventListener("click", toggleModal);
 
-    menuBtnRef.classList.toggle("is-open");
-    mobileMenuRef.classList.toggle("is-open");
-    
-    });
+  function toggleModal() {
+    refs.modal.classList.toggle("is-open");
+    refs.modal_btn.classList.toggle("is-open");
+  }
 })();
 
 $(document).ready(function () {
@@ -25,9 +38,5 @@ $(document).ready(function () {
         touchThreshold: 7,
         dotsClass: "slider-icons__list",
         // appendDots:$(this).siblings('slider-icons__list'),
-
     })
-
-
 });
-
